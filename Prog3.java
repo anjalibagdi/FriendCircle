4feb1ea3c72d4db83c70dbaa5f2f5388aaef6cba
anjali <anjali@gmail.com>
2024-09-01 19:50:09 +0530

    principal

--- Prog3.java
+++ Prog3.java
@@ -5,6 +5,8 @@ class SI
         int p=1000,t=4;
         float r=2.5f,si;
         si=(p*r*t)/100;
+        System.out.println("Principal :"+p);
+        System.out.println(" Time:"+t);
         System.out.println("Simple Intterest :"+si);
     }
 }
